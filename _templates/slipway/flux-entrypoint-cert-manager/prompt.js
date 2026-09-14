4c3330dadcb12207ec71e7f279c2b2617df7fe96
Mark O'Connor <markoconnor@centripetalnetworks.com>
2026-09-14 17:48:42 +0100

    Make the versions configurable

---
+++ _templates/slipway/flux-entrypoint-cert-manager/prompt.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    type: 'input',
+    name: 'certManagerVersion',
+    message: 'What is the Cert Manager version?',
+    default: 'v1.21.2'
+  },
+  {
+    type: 'input',
+    name: 'trustManagerVersion',
+    message: 'What is the Trust Manager version?',
+    default: 'v0.25.0'
+  }
+];
---
+++ _templates/slipway/flux-entrypoint-cloudnativepg/prompt.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    type: 'input',
+    name: 'cnpgChartVersion',
+    message: 'What is the CloudNativePG chart version?',
+    default: '0.29.0'
+  },
+  {
+    type: 'input',
+    name: 'barmansPluginVersion',
+    message: 'What is the Barman Plugin version?',
+    default: '0.7.1'
+  }
+];
